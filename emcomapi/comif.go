fa0d90e55d3ef755f85af6022ced2cb33deb724d
Alexandru Geana <alex@alegen.net>
2018-04-05 22:05:24 +0200

    created the peripheral type
    At the basic level, the Gateway and Processor concepts were rather similar. They are both components with identifiers which may, or may not generate emersyx events and which receive events (either from other components or the emersyx code). As a result, I created the Peripheral type which is a supertype for both Gateway and Processor types. In addition, the Receptor type is now standalone and can be implemented or not, on a case-by-case basis.

--- emcomapi/comif.go
+++ emcomapi/comif.go
@@ -4,39 +4,22 @@ import (
 	"io"
 )
 
-// Event is the interface for all events supported by the various emersyx components. Event routers should use this type
-// if they are to support two or more event types. If event routers only support one event type (e.g. emircapi.Message
-// or emtgapi.Update), then they can use that particular type directly.
-type Event interface {
-	// GetSourceIdentifier must return the identifier of the emersyx receptor which generated the event.
-	GetSourceIdentifier() string
-}
-
-// Identifiable is a low-level interface (w.r.t. hierarchy of types in the emersyx framework) for all components which
-// have to be uniquely identifiable. This includes gateways (components which are either resources, receptors or both
-// simultaneously) and processors.
-type Identifiable interface {
-	// GetIdentifier must return the identifier of the component.
-	GetIdentifier() string
-}
-
-// Receptor is the interface for all event receptors part of the emersyx platform. Each receptor component must expose
-// a channel via which events are pushed.
-type Receptor interface {
-	// GetEventsOutChannel must return the channel via which the Receptor implementation pushes Event objects. The
-	// channel is read-only and can not be written to.
-	GetEventsOutChannel() <-chan Event
-}
-
 // The RouterOptions interface specifies the options which can be set on a Router implementation. Each method returns a
 // function, which can apply the appropriate configuration to the Router implementation. Each Router implementation
 // needs to also provide a related RouterOptions implementation. The return values of each method of the RouterOptions
 // implementation must be directly usable as arguments to the Router.SetOptions implementation. Different RouterOptions
 // implementations may not be compatible with the same Router implementation.
 type RouterOptions interface {
+	// Core must set the emersyx core instance to be used when requiring its services.
+	Core(core Core) func(Router) error
+	// Logging must set the io.Writer where messages are written to and the logging verbosity level.
 	Logging(writer io.Writer, level uint) func(Router) error
-	Gateways(gws ...Gateway) func(Router) error
-	Processors(procs ...Processor) func(Router) error
+	// Gateways must set the emersyx gateway components to route events from.
+	Gateways(gws ...Peripheral) func(Router) error
+	// Processors must set the emersyx processor components to route events to.
+	Processors(procs ...Peripheral) func(Router) error
+	// Routes must set the links through which events are sent from sources to destinations. Sources can be either
+	// gateways or processors, whereas destinations can only be processors.
 	Routes(routes map[string][]string) func(Router) error
 }
 
@@ -45,47 +28,59 @@ type RouterOptions interface {
 type Router interface {
 	// SetOptions sets the router options given as arguments. These are options implemented via RouterOptions.
 	SetOptions(options ...func(Router) error) error
-	// GetGateway must search for the Identifiable object loaded with the Gateways option and with the specified
-	// identifier. If such an object is found, then it must be returned.
-	GetGateway(id string) (Identifiable, error)
-	// Run must start a loop in which events coming from 1a) the Identifiable objects 2a) loaded via the Gateways option
-	// and 3a) which also implement the Receptor interface are routed to 1b) the Processor objects 2b) loaded via the
-	// Processors option. The events should follow the routes specified via the Routes option. The mentioned options
-	// refer to the ones in the emcomapi.RouterOptions interface.
+	// Run must start a loop in which events are forwarded via the routes specified by the RouterOptions.Routes option.
 	Run() error
 }
 
-// The ProcessorOptions interface specifies the options which can be set on a Processor implementation. Each method
-// returns a function, which can apply the appropriate configuration to the Processor implementation. Each Processor
-// implementation needs to also provide a related ProcessorOptions implementation. The return values of each method of
-// the ProcessorOptions implementation must be directly usable as arguments to the NewProcessor implementation.
-// Different ProcessorOptions implementations may not be compatible with the same Processor implementation.
-type ProcessorOptions interface {
-	Logging(writer io.Writer, level uint) func(Processor) error
-	Identifier(id string) func(Processor) error
-	Config(cfg string) func(Processor) error
-	Router(rtr Router) func(Processor) error
+// Core is the interface which must be implemented by the emersyx core component. This interface defines all the core
+// services which can be performed for any emersyx component.
+type Core interface {
+	// GetPeripheral must search for the Peripheral object loaded by the emersyx core. If such an instance is found,
+	// then it must be returned.
+	GetPeripheral(id string) (Peripheral, error)
 }
 
-// Processor is the interface for all event processors part of the emersyx platform. Each processor component must
-// implement the Identifiable interface and implement a method via which new Event objects are received for processing.
-// Optionally, specific Processor implementations may choose to also implement the Receptor interface if they generate
-// events of their own. Such Processor implementations will be automatically picked up by the emersyx router component.
-type Processor interface {
-	Identifiable
+// Event is the interface for all events supported by the various emersyx components. Event routers should use this type
+// if they are to support two or more event types. If event routers only support one event type (e.g. emircapi.Message
+// or emtgapi.Update), then they can use that particular type directly.
+type Event interface {
+	// GetSourceIdentifier must return the identifier of the emersyx receptor which generated the event.
+	GetSourceIdentifier() string
+}
+
+// The PeripheralOptions interface specifies the options which can be set on a Peripheral implementation. Each method
+// returns a function, which can apply the appropriate configuration to the Peripheral implementation. Each Peripheral
+// implementation needs to also provide a related PeripheralOptions implementation. The return values of each method of
+// the PeripheralOptions implementation must be directly usable as arguments to the NewPeripheral implementation.
+// Different PeripheralOptions implementations may not be compatible with the same Peripheral implementation.
+type PeripheralOptions interface {
+	// Logging must set the io.Writer where messages are written to and the logging verbosity level.
+	Logging(writer io.Writer, level uint) func(Peripheral) error
+	// Identifier must set the unique ID string for the Peripheral instance.
+	Identifier(id string) func(Peripheral) error
+	// Core must set the emersyx core instance to be used when requiring its services.
+	Core(core Core) func(Peripheral) error
+	// Config sets the path to the configuration file from which the peripheral instance loads additional configuration
+	// options.
+	Config(cfg string) func(Peripheral) error
+}
+
+// Peripheral is a low-level interface (w.r.t. hierarchy of types in the emersyx framework) for all components which
+// have to be uniquely identifiable. This includes gateways (components which are either resources, receptors or both
+// simultaneously) and processors.
+type Peripheral interface {
+	// GetIdentifier must return the identifier of the peripheral.
+	GetIdentifier() string
 	// GetEventsInChannel must return the channel via which the Processor implementation receives Event objects. The
 	// channel is write-only and can not be read from.
 	GetEventsInChannel() chan<- Event
 }
 
-// Gateway is the interface for all service gateways part of the emersyx platform. Each gateway component must implement
-// the Identifiable and Receptor interfaces, and a method via which new Event objects are pushed for processing by event
-// processors.
-type Gateway interface {
-	Identifiable
-	Receptor
-	// GetEventsInChannel must return the channel via which the Gateway implementation receives CoreEvent objects.
-	// Gateways are meant to receive events only from the emersyx core. The channel is write-only and can not be read
-	// from.
-	GetEventsInChannel() chan<- CoreEvent
+// Receptor is the interface for all event receptors part of the emersyx platform. Each receptor component must expose
+// a channel via which events are pushed. An emersyx component may implement the Receptor interface (next to the
+// Peripheral interface) if the component can capture events.
+type Receptor interface {
+	// GetEventsOutChannel must return the channel via which the Receptor implementation pushes Event objects. The
+	// channel is read-only and can not be written to.
+	GetEventsOutChannel() <-chan Event
 }
