fecf7496ed6b0f4e12b7e6a75c584e51e5d751c8
Alexandru Geana <alex@alegen.net>
2018-04-05 22:20:10 +0200

    IRC api consolidation
    The contents of the message.go file are moved to the ircif.go file. Furthermore, the IRCGateway interface is updated according to the new common Peripheral interface.

--- emircapi/ircif.go
+++ emircapi/ircif.go
@@ -4,23 +4,36 @@ import (
 	"emersyx.net/emersyx_apis/emcomapi"
 )
 
-// IRCResource is the interface which specifies the methods that must be implemented by any supported IRC resource.
-// These methods have to be implemented by an IRCReceptor type in order to become an IRCGateway type.
-type IRCResource interface {
-	// Join method must send a JOIN command to the IRC server. The argument specifies the channel to be joined. If the
-	// instance is not connected to any IRC server, then an error is returned.
-	Join(channel string) error
-	// Privmsg must send a PRIVMSG command to the IRC server. The first argument specifies the destination (i.e.  either
-	// a user or a channel) and the second argument is the actual message. If the instance is not connected to any IRC
-	// server, then an error is returned.
-	Privmsg(destination string, message string) error
+// These constant values must be used for the Command member of the Message struct.
+const (
+	DISCONNECTED = "DISCONNECTED"
+	JOIN         = "JOIN"
+	PART         = "PART"
+	PRIVMSG      = "PRIVMSG"
+	QUIT         = "QUIT"
+)
+
+// IRCMessage is the basic structure for an IRC message received by the client when an event occurs. Names of the struct
+// members have been taken from RFC-1459 and RFC-2812. This is the structure which implements the emcomapi.Event
+// interface for IRC events.
+type IRCMessage struct {
+	Source     string
+	Raw        string
+	Command    string
+	Origin     string
+	Parameters []string
 }
 
-// IRCGateway is the interface which specifies the methods that must be implemented by a complete IRC receptor and resource.
-// The reference implementation at https://github.com/emersyx/emersyx_irc follows this interface.
+// GetSourceIdentifier returns the identifier of the IRCGateway instance which generated the emersyx event.
+func (m Message) GetSourceIdentifier() string {
+	return m.Source
+}
+
+// IRCGateway is the interface which for an IRC peripheral and receptor. The reference implementation at
+// https://github.com/emersyx/emersyx_irc follows this interface.
 type IRCGateway interface {
-	emcomapi.Gateway
-	IRCResource
+	emcomapi.Peripheral
+	emcomapi.Receptor
 	// Connect must start the connection process to the selected IRC server. This must be a blocking method. When the
 	// method returns, the IRCResource must connected to the IRC server if the return value is nil. Otherwise, it is
 	// considered that an error occurred and the connection was not possible.
@@ -30,4 +43,11 @@ type IRCGateway interface {
 	// Quit must disconnect the IRCResource from the IRC server. This must be a blocking method. When the method
 	// returns, the instance must not be connected to the IRC server anymore.
 	Quit() error
+	// Join method must send a JOIN command to the IRC server. The argument specifies the channel to be joined. If the
+	// instance is not connected to any IRC server, then an error is returned.
+	Join(channel string) error
+	// Privmsg must send a PRIVMSG command to the IRC server. The first argument specifies the destination (i.e.  either
+	// a user or a channel) and the second argument is the actual message. If the instance is not connected to any IRC
+	// server, then an error is returned.
+	Privmsg(destination string, message string) error
 }
